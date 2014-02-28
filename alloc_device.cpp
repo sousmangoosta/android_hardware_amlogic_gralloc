8a7cd0a7b5058b22f22844641c7c0331d764a81e
jiyu.yang <jiyu.yang@amlogic.com>
2014-02-28 21:48:28 +0800

    PD#87664 height's 16byte align for camera ION mode
    commit 91b551b6c2e46014709519264627e96a620b0beb
    Author: jiyu.yang <jiyu.yang@amlogic.com>
    Date:   Fri Feb 28 15:33:25 2014 +0800
    
        PD#87664 fix YV12 align bug
    
    commit 6f3840332afc4053fd0a1e2f9dd5122a41bc3aaf
    Author: jiyu.yang <jiyu.yang@amlogic.com>
    Date:   Fri Feb 28 10:03:12 2014 +0800
    
        PD#87664 height's 16byte align for camera ION mode

--- alloc_device.cpp
+++ alloc_device.cpp
@@ -334,13 +334,40 @@ static int alloc_device_alloc(alloc_device_t* dev, int w, int h, int format, int
 		switch (format)
 		{
 			case HAL_PIXEL_FORMAT_YCrCb_420_SP:
-			case HAL_PIXEL_FORMAT_YV12:
 				stride = GRALLOC_ALIGN(w, 16);
 			if (usage & GRALLOC_USAGE_PRIVATE_1){
-				size = h * (GRALLOC_ALIGN(w, 32) + GRALLOC_ALIGN(stride/2,16));
+
+                                /************************************************************
+                                 *
+                                 * height is 16bytes aligned for encode canvas read
+                                 *
+                                 * with 16bytes align. width is 32bytes aligned for
+                                 *
+                                 * ge2d working with 256bit once that is 32bytes
+                                 *
+                                 ************************************************************/
+				size = GRALLOC_ALIGN(h, 16) * (GRALLOC_ALIGN(w, 32) + GRALLOC_ALIGN(stride/2,16));
 			}else {
 				size = h * (stride + GRALLOC_ALIGN(stride/2,16));
 			}
+				break;
+			case HAL_PIXEL_FORMAT_YV12:
+                                stride = GRALLOC_ALIGN(w, 16);
+                                if (usage & GRALLOC_USAGE_PRIVATE_1){
+
+                                        /************************************************************
+                                         *
+                                         * height is 16bytes aligned for encode canvas read
+                                         *
+                                         * with 16bytes align. width is 32bytes aligned for
+                                         *
+                                         * ge2d working with 256bit once that is 32bytes
+                                         *
+                                         ************************************************************/
+                                        size = GRALLOC_ALIGN(h, 16) * (GRALLOC_ALIGN(w, 64) + GRALLOC_ALIGN(stride/2,32));
+                                }else {
+                                        size = h * (stride + GRALLOC_ALIGN(stride/2,16));
+                                }
 				break;
 			default:
 				return -EINVAL;
