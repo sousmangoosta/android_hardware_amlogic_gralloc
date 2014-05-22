a9a45bb14fb0be570bfc518d664ea277d56e79ed
jintao.xu <jintao.xu@amlogic.com>
2014-05-22 15:04:10 +0800

    PD #92195: omx 4k2k video display

--- alloc_device.cpp
+++ alloc_device.cpp
@@ -422,6 +422,12 @@ static int alloc_device_alloc(alloc_device_t* dev, int w, int h, int format, int
 		if (usage & GRALLOC_USAGE_AML_VIDEO_OVERLAY)
 		{
 			hnd->flags |= private_handle_t::PRIV_FLAGS_VIDEO_OVERLAY;
+
+		}
+		if (usage & GRALLOC_USAGE_PRIVATE_2)
+		{
+			private_handle_t* hnd = (private_handle_t*)(*pHandle);
+			hnd->flags |= private_handle_t::PRIV_FLAGS_VIDEO_OMX;
 		}
 	}
 
